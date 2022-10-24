4adca1c6158084ceced3007315849e8bd92b3868
Ehtisham Ali <65615524+ehtishamaliawan@users.noreply.github.com>
2022-10-24 18:18:59 +0500

    Update YouTubeFrontEnd.js

--- YouTube Profile Picture Downloader/YouTubeFrontEnd.js
+++ YouTube Profile Picture Downloader/YouTubeFrontEnd.js
@@ -1,13 +1,13 @@
  $(document).ready(function(){
                 $('#subButton').click(function(){
                     const a = $('#urlerId').val()
+                    $("#changeImg").attr("src", 'https://esymi5ndv4y.exactdn.com/wp-content/uploads/2022/10/loading-spinner.webp');
     var final = `https://bv23xuvq26.execute-api.us-east-2.amazonaws.com/youtube-profile-download?urler=${a}`
                     receive_change_image(final);
                 });
             });
 
 async function receive_change_image(url) {
-    $("#changeImg").attr("src", 'https://esymi5ndv4y.exactdn.com/wp-content/uploads/2022/10/loading-spinner.webp');
     let obj;
     const res = await fetch(url)
     obj = await res.json();
